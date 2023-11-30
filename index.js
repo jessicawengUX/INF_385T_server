68fefb70dd8a223156266457831d521e333bd663
Jessica Weng <143017544+jessicawengUX@users.noreply.github.com>
2023-11-30 11:29:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const cors = require("cors");
-
-// require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: "./config.env" });
 
 const port = process.env.PORT || 5050;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/routes"));
+
+// Serve the landing.html file
+app.use(express.static(path.join(__dirname, '../client/src')));
+
+app.get('/', (req, res) => {
+ res.sendFile(path.join(__dirname, '../client/src/landing.html'));
+});
+
 // get driver connection
-// const dbo = require("./database/conn");
+const dbo = require("./database/conn");
 
 app.listen(port, () => {
     // perform a database connection when server starts
